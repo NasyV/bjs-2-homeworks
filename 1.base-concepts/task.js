3ebaaca49d6ef119f371ca327330271de5227fba
Валькова Анастасия <ms.trinity02@mail.ru>
2023-04-20 13:32:39 +0500

    Первое задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2 - 4 * a * c;
+  if (d < 0) {
+    arr;
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+    console.log(arr)
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = (percent/100)/12; 
+  let s = amount - contribution; 
+  let paymentOnMonth = s * (p + (p / (((1 + p) ** countMonths) - 1))); 
+  let totalDebt = (countMonths * paymentOnMonth).toFixed(2); 
+  return Number(totalDebt);
 }
+calculateTotalMortgage(10, 0, 20000, 24);
