3a1d44fcca3d806ea30c852bd8381a6f400a58b5
NasyV <ms.trinity02@mail.ru>
2023-05-17 20:55:05 +0500

    Шестое задание выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,56 @@
-﻿
+﻿//ЗАДАЧА 1
+function parseCount(value) {
+    let parseResult = Number.parseFloat(value);
+    if(Number.isNaN(parseResult)) {
+      throw new Error ("Невалидное значение");
+    } 
+      return parseResult;
+  } 
+  
+  function validateCount(value) {
+    let parseResult;
+    try {
+      parseResult = parseCount(value);
+      return parseResult;
+    } catch (error) {
+      return error;
+    }
+  }
+
+//ЗАДАЧА 2
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    get perimeter() {
+        return this.a+this.b+this.c;
+    }
+    get area() {
+        let p = this.perimeter / 2;
+        let area = Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c));
+        return parseFloat(area.toFixed(3));
+    }
+   
+}
+
+function getTriangle(a, b, c) {
+  let d 
+  try {
+    d = new Triangle(a, b, c);
+    return d;
+  } catch (error) {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      }
+    }    
+  }
+}
