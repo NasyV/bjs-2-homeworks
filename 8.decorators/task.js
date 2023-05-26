e752265fb3bdf3cb24f321330c252523fd5646f9
NasyV <ms.trinity02@mail.ru>
2023-05-26 15:59:38 +0500

    Восьмое задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,55 @@
-//Задача № 1
+//ЗАДАЧА №1
 function cachingDecoratorNew(func) {
+    let cache = [];
   
-}
+    function wrapper(...args) {
+      const hash = md5(args); 
+      let objectInCache = cache.find((item) => item.hash === hash); 
+      let index = cache.findIndex((arr) => arr.hash === hash);
+      if (objectInCache) {
+        console.log(`Из кэша: ${cache[index].result}`); 
+        return `Из кэша: ${cache[index].result}`;
+      }
+  
+      let result = func(...args); 
+      cache.push({ hash, result });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+    return wrapper;
+  }
+  
+  //ЗАДАЧА №2
+  function debounceDecoratorNew(func, delay) {
+    let timeoutId = null;
+    function wrapper(...args) {
+      if (timeoutId === null) {
+        console.log(func.call(this, ...args));
+        console.log("вызвали first колбек");
+        wrapper.count++;
+      }
+      if (timeoutId) {
+        console.log("удалили текущий таймаут");
+        clearTimeout(timeoutId);
+      }
+      console.log("создаем новый таймаут");
+      timeoutId = setTimeout(
+        () => {
+          timeoutId = true;
+          console.log(func(...args));
+          wrapper.count++;
+          console.log("вызвали колбек");
+        },
+        delay,
+        wrapper.allCount++
+      );
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
+  }
 
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
-}
+
